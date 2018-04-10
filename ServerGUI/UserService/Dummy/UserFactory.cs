6bfe66c1fc0052347f23a3a2b9e889993bbbac9b
Unknown <rtomilobil@gmail.com>
2018-04-10 22:30:54 +0200

    add missing @ from dummy email

--- ServerGUI/UserService/Dummy/UserFactory.cs
+++ ServerGUI/UserService/Dummy/UserFactory.cs
@@ -22,7 +22,7 @@ namespace UserService.Dummy
             {
                 DataRow row = Table.NewRow();
                 row[Columns[0]] = String.Format("{0} {1}", firstNames[i], lastNames[i]);
-                row[Columns[1]] = String.Format("{0} {1}{2}", firstNames[i], lastNames[i], "gmail.com");
+                row[Columns[1]] = String.Format("{0} {1}{2}", firstNames[i], lastNames[i], "@gmail.com");
                 row[Columns[2]] = "123";
                 row["RegistrationDate"] = DateTime.Now;
                 Table.Rows.Add(row);
