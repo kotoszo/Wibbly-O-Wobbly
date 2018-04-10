f65a1fecc83d2bf89569481a1d182c8507a89c80
Unknown <rtomilobil@gmail.com>
2018-04-10 18:17:24 +0200

    init userservice

---
+++ ServerGUI/UserService/IUserService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using UserService.Model;
+
+namespace UserService
+{
+    [ServiceContract]
+    public interface IUserService
+    {
+        [OperationContract]
+        User GetUser(string name, string password);
+
+        [OperationContract]
+        void NewUser(string name, string password, string email, DateTime regDate);
+        
+        // prop Connection
+    }
+}
---
+++ ServerGUI/UserService/UserService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using UserService.Model;
+
+namespace UserService
+{
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    public class UserService : IUserService
+    {
+        // prop to DbHandler
+
+        public UserService()
+        {
+
+        } 
+        /// when want to login
+        public User GetUser(string name, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void NewUser(string name, string password, string email, DateTime regDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
