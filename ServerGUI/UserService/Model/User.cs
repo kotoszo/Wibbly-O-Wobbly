f513ad47b07346a2c9037c34af4650a7c1f25152
Unknown <rtomilobil@gmail.com>
2018-04-11 19:36:05 +0200

    add paramless constructor and tostring

--- ServerGUI/UserService/Model/User.cs
+++ ServerGUI/UserService/Model/User.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
 using System.Runtime.Serialization;
 
 namespace UserService.Model
 {
-    [DataContract]
+    //[DataContract] //with this, we get an empty user object in the Hub
     public class User
     {
-        public int Id { get; private set; }
-        public string Name { get; private set; }
-        public string Email { get; private set; }
-        public DateTime RegDate { get; private set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime RegDate { get; set; }
+        
+        public User()
+        {
+
+        }
 
         public User(int id, string name, string email, DateTime regDate)
         {
@@ -17,6 +24,11 @@ namespace UserService.Model
             Name = name;
             Email = email;
             RegDate = regDate;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1} {2}", Name, Email, RegDate);
         }
     }
 }
