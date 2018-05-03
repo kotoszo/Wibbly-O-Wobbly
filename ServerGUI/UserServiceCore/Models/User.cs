3b09ca17755310a66672373fc90b9defe483518a
Peter Korossy <meanwhile@vipmail.hu>
2018-05-03 11:56:51 +0200

    User fixed

--- ServerGUI/UserServiceCore/Models/User.cs
+++ ServerGUI/UserServiceCore/Models/User.cs
@@ -8,17 +8,28 @@ namespace UserServiceCore.Models
 {
     public class User
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        public DateTime RegDate { get; set; }
         public string Password { get; set; }
-    }
-    public class WebApiDataContext : DbContext
-    {
 
-        public DbSet<User> Users { get; set; }
-
-        public WebApiDataContext(DbContextOptions<WebApiDataContext> options) : base(options)
+        public User(int id, string name, string email, DateTime regTime, string password = "kamupw")
         {
+            Id = id;
+            Name = name;
+            Email = email;
+            RegDate = regTime;
+            Password = password;
         }
     }
+    //public class WebApiDataContext : DbContext
+    //{
+
+    //    public DbSet<User> Users { get; set; }
+
+    //    public WebApiDataContext(DbContextOptions<WebApiDataContext> options) : base(options)
+    //    {
+    //    }
+    //}
 }
