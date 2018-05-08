3c9ddb467da1f2d83fe7586fe3c9b1a5a4662120
Peter Korossy <meanwhile@vipmail.hu>
2018-05-08 13:00:08 +0200

    Models with context

---
+++ ConsoleApp1/BikeItemModel/BikeItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeItemModel
+{
+    public class BikeItem
+    {
+        public int Id { get; set; }
+        public string BikeName { get; set; }
+        public float Price { get; set; }
+        public string BikeType { get; set; }
+
+        public int SupplierId { get; set; }
+        public Supplier Supplier { get; set; }
+    }
+}
