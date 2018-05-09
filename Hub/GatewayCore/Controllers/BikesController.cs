33bead82f4d1f3e8ece9411e2e80b8d3c39315bf
Peter Korossy <meanwhile@vipmail.hu>
2018-05-09 13:59:55 +0200

    Init BikesController

---
+++ Hub/GatewayCore/Controllers/BikesController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GatewayCore.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Bikes")]
+    public class BikesController : Controller
+    {
+    }
+}
