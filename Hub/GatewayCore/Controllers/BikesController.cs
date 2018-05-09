d4809fa919c8e16f540a91485a15884803ebfeec
Peter Korossy <meanwhile@vipmail.hu>
2018-05-09 14:16:19 +0200

    GetBikes
    getAll and getById

--- Hub/GatewayCore/Controllers/BikesController.cs
+++ Hub/GatewayCore/Controllers/BikesController.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace GatewayCore.Controllers
 {
-    [Produces("application/json")]
-    [Route("api/Bikes")]
+    //[Produces("application/json")]
+
+    [Route("api/[controller]")]
     public class BikesController : Controller
     {
+        HttpClient client = new HttpClient();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return GetHelper(@"http://localhost:61933/api/bikeitem");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return GetHelper(@"http://localhost:61933/api/bikeitem/" + id);
+        }
+        
+
+        private IActionResult GetHelper(string uri)
+        {
+            HttpResponseMessage response = client.GetAsync(uri).Result;
+            string stringData = response.Content.ReadAsStringAsync().Result;
+            return new ObjectResult(JsonConvert.DeserializeObject(stringData));
+        }
     }
+
 }
