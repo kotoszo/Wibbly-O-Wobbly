3cddfe4441670f49962390ed77b7b73fcdbcaea0
Unknown <rtomilobil@gmail.com>
2018-05-09 13:41:18 +0200

    init orders controller

---
+++ Hub/GatewayCore/Controllers/OrdersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace GatewayCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class OrdersController : Controller
+    {
+        HttpClient client = new HttpClient();
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return GetHelper("http://localhost:49634/api/orders");
+        }
+
+        private IActionResult GetHelper(string uri)
+        {
+            HttpResponseMessage response = client.GetAsync(uri).Result;
+            string stringData = response.Content.ReadAsStringAsync().Result;
+            return new ObjectResult(JsonConvert.DeserializeObject(stringData));
+        }
+    }
+}
